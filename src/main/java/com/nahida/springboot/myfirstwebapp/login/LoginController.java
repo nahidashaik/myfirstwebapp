185a920037aed3bb7a24208218f3ce996f059f37
nahidashaik <92881369+nahidashaik@users.noreply.github.com>
2023-03-25 17:27:17 -0500

    logger slf4j

--- src/main/java/com/nahida/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/nahida/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.nahida.springboot.myfirstwebapp.login;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
  //login redirected to com.nahida.springboot.myfirstwebapp.login.LoginController =>Login.jsp
 //http://localhost:8080/login?name=Nahida
-	
+	private Logger logger=LoggerFactory.getLogger(getClass());
 	//Model
 	@RequestMapping("/login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		System.out.println("request param is  "+name); //Dont do this in production
+		logger.debug("Request Param is {} "+name);
+//		System.out.println("request param is  "+name); //Dont do this in production
 		model.put("name ", name);
 		return "login";
 	} 
